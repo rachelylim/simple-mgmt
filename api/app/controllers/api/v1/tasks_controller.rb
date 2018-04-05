0dd6b52c63849681a79b93152bc688d4510212fa
Rachel Lim <rachel.y.lim@gmail.com>
2018-04-05 14:55:32 -0400

    write create method for task controller.

--- api/app/controllers/api/v1/tasks_controller.rb
+++ api/app/controllers/api/v1/tasks_controller.rb
@@ -2,9 +2,16 @@ module Api
   module V1
     class TasksController < ApplicationController
       def index
+        render json: Task.all
       end
 
       def create
+        @task = Task.create(task_params)
+        if @task.save
+          render json: @task, status: 201
+        else
+          render json: { @task.errors.full_messages }, status: 422
+        end
       end
 
       def update
