969edb21b97eb20bd14da947744ee7c265d3d013
Rachel Lim <rachel.y.lim@gmail.com>
2018-04-05 14:47:15 -0400

    initialize boiler plate react/redux.

---
+++ client/dev/js/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+require('../../scss/style.scss');
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        hello world
+      </div>
+    );
+  }
+}
+
+export default App;
