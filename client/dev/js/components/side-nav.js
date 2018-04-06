e8ca1f7546f0c92c7f6a434d04b50f8067e2446a
Rachel Lim <rachel.y.lim@gmail.com>
2018-04-05 22:00:02 -0400

    create side nav with button to create new task.

--- client/dev/js/components/side-nav.js
+++ client/dev/js/components/side-nav.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import styled, { css } from 'react-emotion';
+
+import Button from '../elements/button';
+
+const Wrapper = styled.div`
+  padding: 15px 25px;
+  border-right: 1px solid black;
+  height: 100%;
+`;
 
 class SideNav extends Component {
   render() {
-    return <div>SideNav</div>
+    return (
+      <Wrapper>
+        <Button copy='+ Create New Task' onclick={this.props.openTaskCreator(true)} />
+      </Wrapper>
+    )
   }
 }
 
