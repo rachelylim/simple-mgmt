29b4e2cc4342b3ef7c44d6a437ce3d327bde25cc
Rachel Lim <rachel.y.lim@gmail.com>
2018-04-05 21:59:48 -0400

    create list component that lists all tasks.

--- client/dev/js/components/list.js
+++ client/dev/js/components/list.js
@@ -1,9 +1,41 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import styled, { css } from 'react-emotion';
 
+import Task from './task';
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+`;
+
+const TaskWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
 class List extends Component {
+  renderTasks() {
+    return (
+      <TaskWrapper>
+        {this.props.tasks.map(task => <Task {...task} key={task.id} />)}
+      </TaskWrapper>
+    )
+  }
+
+  renderEmptyState() {
+    return (
+      <h3>There are no tasks. Get to work!</h3>
+    );
+  }
+
   render() {
-    return <div>List</div>
+    return (
+      <Wrapper>
+        {this.props.tasks && this.props.tasks.length ? this.renderTasks() : this.renderEmptyState()}
+      </Wrapper>
+    );
   }
 }
 
