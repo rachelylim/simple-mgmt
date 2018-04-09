3750da303c79bd49d0764814cd796bc424f10e5e
Rachel Lim <rachel.y.lim@gmail.com>
2018-04-08 22:53:52 -0400

    move package and config files to base of repo.

--- client/dev/js/components/task.js
+++ client/dev/js/components/task.js
@@ -18,7 +18,7 @@ const TaskWrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: flex-start;
-  width: 80%;
+  width: 75%;
   text-decoration: ${props => props.complete ? 'line-through' : 'none'};
 `;
 
