817237236a17658d751d880845bba0e8034376fb
Rachel Lim <rachel.y.lim@gmail.com>
2018-04-10 21:57:10 -0400

    add proptypes to element component.

--- client/dev/js/elements/checkbox.js
+++ client/dev/js/elements/checkbox.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import styled from 'react-emotion';
 
 class Checkbox extends Component {
+  static get propTypes() {
+    return {
+      checked: PropTypes.bool,
+      onchange: PropTypes.func,
+    }
+  }
+
   change = () => {
     this.props.onchange && this.props.onchange();
   }
 
   render() {
-    return <input type='checkbox' onChange={this.change} checked={this.props.checked} />;
+    return (
+      <input
+        type='checkbox'
+        onChange={this.change}
+        checked={this.props.checked}
+      />
+    );
   }
 }
 
